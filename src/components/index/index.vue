72f0033f9c8d9f691fa4af778f1332f7afbdfa56
zhangsong@optaim.com <zhangsong@optaim.com>
2017-03-21 17:52:03 +0800

    2

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -11,19 +11,32 @@
       <div class="tab_search">
         <span @click="nav_search()">搜索</span>
       </div>
-    </div><br />
+    </div><br /><br />
     <div class="comment">
-      <ul class="comment_ul">
-        <li class="comment_li" v-for="item in items"> 
-           <span>{{item}}</span>
-        </li>
-      </ul>
+      <mt-loadmore :top-method="loadTop" :bottom-method="loadBottom" ref="loadmore">
+        <ul class="comment_ul">
+          <li class="comment_li" v-for="item in items" style="list-style: none"> 
+            <router-link :to="{ path:'/index/detail/'+item.id}"> 
+              <h4 class="comment_title">{{ item.title }}</h4>
+              <div class="comment_img" v-if="item.images.length == 3 || item.images.length == 0">
+                  <span class="img-1" v-for="imgurl in item.images">
+                      <img :src="imgurl"/>
+                  </span>
+              </div>
+              <div class="comment_state" v-if="item.images.length == 3 || item.images.length == 0">
+                <span>{{ item.source }}</span><span>{{ item.scanCount }}浏览</span><span>{{ item.publishTime }}</span>
+              </div>
+            </router-link>
+          </li>
+        </ul>
+      </mt-loadmore>
     </div>
   </div>
 </template>
 
 <script>
   import { Loadmore } from 'mint-ui';
+  import detail from './detail.vue';
   export default {
     data() {
       return {
@@ -32,8 +45,19 @@
         items:[]
       }
     },
+    computed() {
+
+    },
     methods: {
-      
+      loadTop() {
+        this.$refs.loadmore.onTopLoaded();
+      },
+      loadBottom(){
+        this.$refs.loadmore.onBottomLoaded();
+      },
+      openDetail(){
+        
+      }
     },
     created() {
       this.$http.get('/api/article').then((response) => {
@@ -48,7 +72,6 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .header
     width:100%
-    height:24px
     line-height:24px
     border-bottom:1px solid #ccc
     overflow-x:scroll
@@ -85,4 +108,31 @@
       z-index:9
       border-left:1px solid #ccc
       font-size:12px
+  .comment
+    width:100%
+    .comment_ul
+      .comment_li
+        border-bottom:1px solid #ccc
+        margin:5px 0
+        padding:0 5px
+        .comment_title
+          text-align:left
+          font-size:16px
+          margin:5px 0
+          font-family:"微软雅黑"
+        .comment_img
+          display:flex
+          width:100%
+          .img-1
+            flex:1
+            img
+              display:inline-block
+              width:90%
+        .comment_state
+          font-size:12px
+          text-align:left
+          margin: 8px 0 10px 5px
+          color:#ddd
+          span
+            margin-right:15px 
 </style>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import MintUi from 'mint-ui';
 import App from './App'
 // import router from './router';
 import index from '@/components/index/index';
+import detail from '@/components/index/detail';
 import movie from '@/components/movie/movie';
 import personal from '@/components/personal/personal';
 
@@ -18,10 +19,13 @@ Vue.use(MintUi);
 
 const routes = [
   { path: '/index/index', component: index },
+  { path: '/index/detail/:id', component: detail },
   { path: '/movie/movie', component: movie },
   { path: '/personal/personal', component: personal }
 ];
 const router = new VueRouter({
+  mode:"history",
+  saveScrollPosition:true,  //在启用html5 history模式的时候生效，用于后退操作的时候记住之前的滚动条位置
   routes,
   linkActiveClass: 'active'
 });
